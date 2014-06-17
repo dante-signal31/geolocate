0651eac5a5931d0cba469fdc2f2a7ccd53d73a7a
Dante Signal31 <dante-signal31@users.noreply.github.com>
2014-06-17 22:06:38 +0200

    TestParser's test_GeolocateInputParser_next() includes checking of geoip.IPNotFound exception. Test coverage of parser.py is now 100%

--- geolocate/classes/geoip.py
+++ geolocate/classes/geoip.py
@@ -1,7 +1,15 @@
+"""
+ geolocate wrapper to Geolite2 API.
+
+ Programmed by: Dante Signal31
+
+ email: dante.signal31@gmail.com
+"""
 
 def load_geoip_database():
     ##TODO: Implement this function.
     return GeoIPDatabase()
+
 
 class GeoIPDatabase(object):
     ##TODO: Implement this class.
@@ -10,3 +18,13 @@ class GeoIPDatabase(object):
 
     def locate(self, ip):
         pass
+
+
+class IPNotFound(Exception):
+
+    def __init__(self, IP):
+        message = "The address {0} is not in the database.".format(IP)
+        Exception.__init__(self, message)
+        self.failed_IP = IP
+
+
