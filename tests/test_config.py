f2e7d636d4a6f5acdee240f3a43f9d9d2009252e
Dante Signal31 <dante.signal31@gmail.com>
2014-06-22 00:37:28 +0200

    Added some test for config.py.

--- tests/test_config.py
+++ tests/test_config.py
@@ -19,8 +19,25 @@ class TestConfiguration(unittest.TestCase):
         wrong_user_id = "john doe" # Spaces don't use to be allowed.
         with self.assertRaises(config.ParameterNotValid) as e:
             configuration = config.Configuration(user_id=wrong_user_id)
+        correct_user_id = "john_doe"
+        try:
+            configuration = config.Configuration(user_id=correct_user_id)
+        except config.ParameterNotValid:
+            self.fail("ParameterNotValid exception with supposedly correct "
+                      "parameter")
 
     def test_license_key_validation(self):
         wrong_license_key = "1234 56ab"
         with self.assertRaises(config.ParameterNotValid) as e:
             configuration = config.Configuration(license_key=wrong_license_key)
+        correct_license_id = "123456ab"
+        try:
+            configuration = config.Configuration(license_key=correct_license_id)
+        except config.ParameterNotValid:
+            self.fail("ParameterNotValid exception with supposedly correct "
+                      "parameter")
+
+    def test_download_url_validation(self):
+        wrong_url = "www-google.com"
+        with self.assertRaises(config.ParameterNotValid) as e:
+            configuration = config.Configuration(download_url=wrong_url)
