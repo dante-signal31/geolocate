13efb1cd545184a9d39157b5c5146001732a553c
Dante Signal31 <dante.signal31@gmail.com>
2014-06-22 11:49:37 +0200

    Config.py unitests refactored.

--- tests/test_config.py
+++ tests/test_config.py
@@ -17,27 +17,31 @@ class TestConfiguration(unittest.TestCase):
 
     def test_user_id_validation(self):
         wrong_user_id = "john doe" # Spaces don't use to be allowed.
-        with self.assertRaises(config.ParameterNotValid) as e:
-            configuration = config.Configuration(user_id=wrong_user_id)
+        self._test_wrong_parameter("user_id", wrong_user_id)
         correct_user_id = "john_doe"
-        try:
-            configuration = config.Configuration(user_id=correct_user_id)
-        except config.ParameterNotValid:
-            self.fail("ParameterNotValid exception with supposedly correct "
-                      "parameter")
+        self._test_correct_parameter("user_id", correct_user_id)
 
     def test_license_key_validation(self):
         wrong_license_key = "1234 56ab"
-        with self.assertRaises(config.ParameterNotValid) as e:
-            configuration = config.Configuration(license_key=wrong_license_key)
-        correct_license_id = "123456ab"
-        try:
-            configuration = config.Configuration(license_key=correct_license_id)
-        except config.ParameterNotValid:
-            self.fail("ParameterNotValid exception with supposedly correct "
-                      "parameter")
+        self._test_wrong_parameter("license_key", wrong_license_key)
+        correct_license_key = "123456ab"
+        self._test_correct_parameter("license_key", correct_license_key)
 
     def test_download_url_validation(self):
         wrong_url = "www-google.com"
+        self._test_wrong_parameter("download_url", wrong_url)
+        correct_url = "www.google.com"
+        self._test_correct_parameter("download_url", correct_url)
+
+    def _test_wrong_parameter(self, parameter, value):
+        arguments_to_pass = {parameter: value, }
         with self.assertRaises(config.ParameterNotValid) as e:
-            configuration = config.Configuration(download_url=wrong_url)
+            configuration = config.Configuration(**arguments_to_pass)
+
+    def _test_correct_parameter(self, parameter, value):
+        arguments_to_pass = {parameter: value, }
+        try:
+            configuration = config.Configuration(**arguments_to_pass)
+        except config.ParameterNotValid:
+            self.fail("ParameterNotValid exception with supposedly correct "
+                      "parameter")
