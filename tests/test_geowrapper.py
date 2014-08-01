65758a732aff38a09fdc6eaac64b96fb81a7c166
Dante Signal31 <dante-signal31@users.noreply.github.com>
2014-08-01 21:02:36 +0200

    test_geowrapper.py created.

---
+++ tests/test_geowrapper.py
@@ -0,0 +1,17 @@
+"""
+ test_geowrapper.py
+
+ Programmed by: Dante Signal31
+
+ email: dante.signal31@gmail.com
+"""
+import unittest
+sys.path.append(os.path.abspath(".."))
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
