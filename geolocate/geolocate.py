d4b54ce4eef983f49f1f22f5067da7830367c52a
Dante Signal31 <dante.signal31@gmail.com>
2015-01-12 14:32:13 +0100

    Geolocate's process_optional_parameter no calls correctly to asigned functions.

--- geolocate/geolocate.py
+++ geolocate/geolocate.py
@@ -14,15 +14,20 @@ Geolocate is possible thanks to `Maxmind GeoIP database <http://www.maxmind.com>
 and their API.
 """
 import argparse
+import sys
 from classes import geowrapper
 from classes import parser
 from classes import config
+from classes import exceptions
 
-optional_functions = ["show_enabled_locators", "set_locators_preference",
-                      "show_disabled_locators", "reset_locators_preference"]
+_must_pass_in_user_arguments = {"show_enabled_locators": False,
+                                "set_locators_preference": True,
+                                "show_disabled_locators": False,
+                                "reset_locators_preference": False}
 
 
 def parse_arguments():
+    # TODO: Include arguments to change default configuration.
     verbosity_choices = parser.GeolocateInputParser.VERBOSITY_LEVELS
     arg_parser = argparse.ArgumentParser(description="Locate IP adresses "
                                                  "in given text.\n")
@@ -49,18 +54,70 @@ def parse_arguments():
     return arg_parser.parse_args()
 
 
+
+def show_enabled_locators():
+    """ Print in console enabled locators ordered by preference.
+
+    :return: None
+    """
+    # TODO: Implement.
+    pass
+
+
+def set_locators_preference(arguments):
+    """ Change locators list with new list given in arguments.
+
+    :param arguments:  Arguments object returned by ArgumentParser.parse_args()
+    :type arguments: Namespace
+    :return: None
+    """
+    # TODO: Implement.
+    pass
+
+
+def show_disabled_locators():
+    """ Print in console enabled locators ordered by preference.
+
+    :return: None
+    """
+    # TODO: Implement.
+    pass
+
+
+def reset_locators_preference():
+    """ Sets back locators list to it's default value.
+
+    :return: None
+    """
+    # TODO: Implement.
+    pass
+
+
 def print_lines_parsed(parser):
     for line in parser:
         print(line, end="")
 
 
 def process_optional_parameters(arguments):
+    """ Take all optional arguments given by user and run one by one all
+    functions assigned to each of them.
+
+    :param arguments: Arguments object returned by ArgumentParser.parse_args()
+    :type arguments: Namespace
+    :return: None
+    """
     # TODO: Locators preference should be stored in configuration between
     # sessions. I have to connect configuration and geowrapper objects.
-    valid_arguments = get_user_arguments(arguments)
+    valid_arguments = _get_user_arguments(arguments)
+    for argument in valid_arguments:
+        try:
+            _execute_function(argument, arguments)
+        except NoFunctionAssignedToArgument as e:
+            sys.exit(". ".join([e.message, "Exiting"]))
+
 
 def _get_user_arguments(arguments):
-    """ Get non public  attributes of arguments object.
+    """ Get public user attributes of arguments object.
 
     Arguments set by user are defined public in object returned by
     ArgumentParser.parse_args(). Built-in attributes in that object are defined
@@ -68,14 +125,73 @@ def _get_user_arguments(arguments):
 
     :param arguments: Arguments object returned by ArgumentParser.parse_args()
     :type arguments: Namespace
-    :return: Public attribute list.
-    :rtype: list
+    :return: Public attribute list of arguments object.
+    :rtype: set
     """
     attribute_list = dir(arguments)
-    public_attributes = [argument for argument in attribute_list if not argument.startswith("_")]
-    return public_attributes
+    public_attributes_set = {argument for argument in attribute_list if not argument.startswith("_")}
+    public_attributes_set = _remove_non_user_attributes(public_attributes_set)
+    return public_attributes_set
 
 
+def _remove_non_user_attributes(attributes_set):
+    """ Takes out from attribute set public attributes not defined by user.
+
+    :param attributes_set: Public attribute list of arguments object.
+    :type attributes_set: set
+    :return: Public attribute list of arguments object without non user attributes.
+    :rtype: set
+    """
+    # Arguments object returned by ArgumentParser.parse_args() include by
+    # default "index" and "count" public attributes. We don't need them so we
+    # strip them off.
+    non_user_attributes = {"index", "count"}
+    return attributes_set.difference(non_user_attributes)
+
+
+def _execute_function(argument, arguments):
+    """ Execute function assigned to given argument.
+
+    :param argument: Argument to search a function assigned for.
+    :param arguments: Arguments object returned by ArgumentParser.parse_args()
+    :type arguments: Namespace
+    :return: None
+    :raise: NoFunctionAssignedToArgument
+    """
+    try:
+        if _must_pass_in_user_arguments[argument]:
+            # Arguments names are equal to names of function to call.
+            _call_function(argument, arguments)
+        else:
+            _call_function(argument)
+    except KeyError:
+        raise NoFunctionAssignedToArgument(argument)
+
+
+def _call_function(function_name, arguments=None):
+    """ Call given function and pass it in arguments if they are given.
+
+    :param function_name: Function to call.
+    :type function_name: str
+    :param arguments:  Arguments object returned by ArgumentParser.parse_args()
+    :type arguments: Namespace
+    :return: None
+    """
+    function = globals().get(function_name)
+    if arguments is None:
+        function()
+    else:
+        function(arguments)
+
+
+class NoFunctionAssignedToArgument(Exception):
+    """ User has given and argument that has no function assigned yet."""
+
+    def __init__(self, argument):
+        self.argument = argument
+        self.message = "Sorry, I don't know what to do " \
+                       "with argument: {0}".format(self.argument)
+        Exception.__init__(self, self.message)
 
 if __name__ == "__main__":
     arguments = parse_arguments()
--- tests/test_geolocate.py
+++ tests/test_geolocate.py
@@ -12,34 +12,84 @@ import unittest
 
 import geolocate.geolocate as geolocate
 
+
+ERRONEOUS_ARGUMENT = "erroneous_argument"
+
 Arguments = namedtuple("Arguments", "show_enabled_locators "
                                     "set_locators_preference "
                                     "show_disabled_locators "
                                     "reset_locators_preference ")
+
+ErroneousArguments = namedtuple("ErroneousArguments",
+                                " ".join(["show_enabled_locators "
+                                          "set_locators_preference "
+                                          "show_disabled_locators "
+                                          "reset_locators_preference ",
+                                          ERRONEOUS_ARGUMENT]))
+
+class MockedArguments(object):
+    """ Used to detect private attributes.
+
+    I couldn't use namedtuple because it doesn't admit attributes prefixed
+    by "_".
+    """
+
+    def __init__(self, arg1, arg2, arg3, arg4):
+        self._priv1 = False
+        self.__priv2 = False
+        self.show_enabled_locators = arg1
+        self.set_locators_preference = arg2
+        self.show_disabled_locators = arg3
+        self.reset_locators_preference = arg4
 
 
 class TestGeoLocate(unittest.TestCase):
 
     def test_process_optional_parameters_show_enabled_locators(self):
         arguments = Arguments(True, None, False, False)
-        _assert_geolocate_function_called("show_enabled_locators", arguments)
+        result = _assert_geolocate_function_called("show_enabled_locators",
+                                                   arguments)
+        self.assertTrue(result)
 
     def test_process_optional_parameters_set_locators_preference(self):
         arguments = Arguments(False, ["geoip2_local", "geoip2_webservice"],
                               False, False)
-        _assert_geolocate_function_called("set_locators_preference", arguments)
+        result = _assert_geolocate_function_called("set_locators_preference",
+                                                   arguments)
+        self.assertTrue(result)
 
     def test_process_optional_parameters_show_disabled_locators(self):
         arguments = Arguments(False, None, True, False)
-        _assert_geolocate_function_called("show_disabled_locators", arguments)
+        result = _assert_geolocate_function_called("show_disabled_locators",
+                                                   arguments)
+        self.assertTrue(result)
 
     def test_process_optional_parameters_reset_locators_preference(self):
         arguments = Arguments(False, None, False, True)
-        _assert_geolocate_function_called("reset_locators_preference",
-                                          arguments)
+        result = _assert_geolocate_function_called("reset_locators_preference",
+                                                   arguments)
+        self.assertTrue(result)
+
+    def tests_process_optional_parameters_erroneos_argument(self):
+        arguments = ErroneousArguments(False, None, False, False, True)
+        with self.assertRaises(geolocate.NoFunctionAssignedToArgument) as e:
+            geolocate._execute_function(ERRONEOUS_ARGUMENT, arguments)
+        self.assertEqual(e.exception.argument, ERRONEOUS_ARGUMENT)
+
+
+    def test_get_user_attributes(self):
+        correct_arguments_set = {"show_enabled_locators",
+                                 "set_locators_preference",
+                                 "show_disabled_locators",
+                                 "reset_locators_preference"}
+        test_arguments_object = MockedArguments(True, True, True, True)
+        valid_arguments = geolocate._get_user_arguments(test_arguments_object)
+        self.assertEqual(valid_arguments, correct_arguments_set)
+
 
 
 def _assert_geolocate_function_called(function_name, arguments):
-    with patch(geolocate, function_name) as mocked_function:
+    target_to_patch = ".".join(["geolocate.geolocate", function_name])
+    with patch(target_to_patch) as mocked_function:
         geolocate.process_optional_parameters(arguments)
-    mocked_function.assert_any_call()
+    return mocked_function.called
