361c6a6c6a921b903c49534566c6ae1963b6e93b
Dante Signal31 <dante.signal31@gmail.com>
2015-03-14 09:58:00 +0100

    Updates to setup.py comments and description text.

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup, find_packages  # Always prefer setuptools over distutils
 
 long_description = """This program accepts any text and searchs inside every IP address. With
-each of those IP addresses, geolocate queries `Maxmind GeoIP database <http://www.maxmind.com>`_
+each of those IP addresses, geolocate queries Maxmind GeoIP database (http://www.maxmind.com)_
 to look for the city and country where IP address or URL is located.
 
 Geolocate is designed to be used in console with pipes and redirections along
 with applications like traceroute, etc. Geolocate's output is the same text
 than input but IP addresses are going to have appended its country and city and long-lat
 coordinates (depending on verbosity level).
+
+More info in: https://bitbucket.org/dante_signal31/geolocate
 """
 
 setup(name="geolocate",
@@ -40,7 +42,8 @@ setup(name="geolocate",
       # TODO: This exclude is not working, tests package is still included in
       # packages. It's a bug in pip:
       #     https://bitbucket.org/pypa/wheel/issue/99/cannot-exclude-directory
-      # Until it is fixed, the workaround is to compile package in two steps:
+      # Until it is fixed, the workaround is to create a MANIFEST.in file where
+      # you prune your undesired files and compile package in two steps:
       #     python setup.py sdist
       #     pip wheel --no-index --no-deps --wheel-dir dist dist/*.tar.gz
       packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*",
