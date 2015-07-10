69544ab888ef0386fef62e88ce1976010ef6be02
dante-signal31 <dante.signal31@gmail.com>
2015-07-11 01:49:01 +0200

    Updated version.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ More info in: https://github.com/dante-signal31/geolocate
 """
 
 setup(name="glocate",
-      version="1.2.3",
+      version="1.3.0s",
       description="This program scans given text to find urls and IP addresses. "
                   "The output is the same text but every url and IP address "
                   "is going to have its geolocation data appended.",
