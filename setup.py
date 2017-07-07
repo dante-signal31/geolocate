b20ad76844d878ba02d73666d08b46cc22f061fd
dante-signal31 <dante.signal31@gmail.com>
2017-07-07 10:46:45 +0200

    Dependencies updated for keyring support.

--- setup.py
+++ setup.py
@@ -38,7 +38,8 @@ setup(name="glocate",
                    'Programming Language :: Python :: 3.4'],
       keywords="geolocation ip addresses",
       install_requires=["geoip2>=2.1.0", "maxminddb>=1.1.1", "requests>=2.5.0",
-                        "wget>=2.2"],
+                        "wget>=2.2", "wheel>=0.24.0", "keyring>=10.4.0",
+                        "dbus-python>=1.2.4"],
       zip_safe=False,
       # TODO: This exclude is not working when building wheels, tests package
       # is still included in packages. It's a bug in pip:
